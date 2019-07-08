1877e2e7d3c20c7a02d2678992c25787a548239f
Logan Garland <legarland@gmail.com>
2019-07-08 12:06:36 -0700

    added purgecss

--- client/src/postcss.config.js
+++ client/src/postcss.config.js
@@ -1,7 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
 const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.jsx', './src/**/*.js', './public/index.html'],
+  css: ['./src/style/tailwind.css'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 
 module.exports = {
-  plugins: [tailwindcss('./src/tailwind.js'), require('autoprefixer')]
+  plugins: [
+    tailwindcss('./src/tailwind.js'),
+    autoprefixer,
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ]
 }
