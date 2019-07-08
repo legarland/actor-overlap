3c0605b11f450d5694aa45797293a39e34b35399
Logan Garland <legarland@gmail.com>
2019-07-08 11:08:26 -0700

    decrease leading

--- client/src/style/character-name.js
+++ client/src/style/character-name.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CharacterName = props => (
   <div
     {...props}
-    className="font-bold my-1 uppercase text-gray-900 text-center tracking-wide"
+    className="font-bold my-1 uppercase text-gray-900 text-center tracking-wide leading-tight"
   />
 )
 
