09d2b202d2f01d5a2cd90a1a6b21e8ed43c01b3b
Logan Garland <legarland@gmail.com>
2019-07-08 11:13:19 -0700

    Imdb links (#4)
    * add imdb links
    
    * external link icon
    
    * eslint fix
    
    * add link to imdb actor page
    
    * decrease leading

--- client/src/components/overlap.js
+++ client/src/components/overlap.js
@@ -11,55 +11,51 @@ import AniamtedReel from './animated-reel'
 const ActorOverlap = () => {
   const [loading, setLoading] = useState(false)
   const [searched, setSearched] = useState(false)
-  const [first, setFirst] = useState(
-    // {
-    //   title: 'Holes',
-    //   id: 'tt0311289',
-    //   image:
-    //     'https://m.media-amazon.com/images/M/MV5BMTg0MTU5ODkwM15BMl5BanBnXkFtZTYwMzgxNzY3._V1_.jpg'
-    // }
-    )
-  const [second, setSecond] = useState(
-    // {
-    //   title: 'Transformers',
-    //   id: 'tt0418279',
-    //   image:
-    //     'https://m.media-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg'
-    // }
-    )
+  const [first, setFirst] = useState({
+    title: 'Holes',
+    id: 'tt0311289',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMTg0MTU5ODkwM15BMl5BanBnXkFtZTYwMzgxNzY3._V1_.jpg'
+  })
+  const [second, setSecond] = useState({
+    title: 'Transformers',
+    id: 'tt0418279',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg'
+  })
   const [data, setData] = useState([
-    // {
-    //   name: 'Shia LaBeouf',
-    //   id: 'nm0479471',
-    //   image:
-    //     'https://m.media-amazon.com/images/M/MV5BMjFhNTM3NDItMDllNC00YzAyLThmY2QtYmViMjBjODJjODdiXkEyXkFqcGdeQXVyMDU5Njk5MQ@@._V1_UY209_CR4,0,140,209_AL_.jpg',
-    //   characters: {
-    //     tt0311289: {
-    //       name: 'Stanley',
-    //       link: '/title/tt0311289/characters/nm0479471?ref_=ttfc_fc_cl_t4'
-    //     },
-    //     tt0418279: {
-    //       name: 'Sam Witwicky',
-    //       link: '/title/tt0418279/characters/nm0479471?ref_=ttfc_fc_cl_t1'
-    //     }
-    //   }
-    // },
-    // {
-    //   name: 'Jon Voight',
-    //   id: 'nm0000685',
-    //   image:
-    //     'https://m.media-amazon.com/images/M/MV5BMTc2NTE3NDA3M15BMl5BanBnXkFtZTgwMDMyNTM1MjE@._V1_UY209_CR3,0,140,209_AL_.jpg',
-    //   characters: {
-    //     tt0311289: {
-    //       name: 'Mr. Sir',
-    //       link: '/title/tt0311289/characters/nm0000685?ref_=ttfc_fc_cl_t2'
-    //     },
-    //     tt0418279: {
-    //       name: 'Defense Secretary John Keller',
-    //       link: '/title/tt0418279/characters/nm0000685?ref_=ttfc_fc_cl_t7'
-    //     }
-    //   }
-    // }
+    {
+      name: 'Shia LaBeouf',
+      id: 'nm0479471',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BMjFhNTM3NDItMDllNC00YzAyLThmY2QtYmViMjBjODJjODdiXkEyXkFqcGdeQXVyMDU5Njk5MQ@@._V1_UY209_CR4,0,140,209_AL_.jpg',
+      characters: {
+        tt0311289: {
+          name: 'Stanley',
+          link: '/title/tt0311289/characters/nm0479471?ref_=ttfc_fc_cl_t4'
+        },
+        tt0418279: {
+          name: 'Sam Witwicky',
+          link: '/title/tt0418279/characters/nm0479471?ref_=ttfc_fc_cl_t1'
+        }
+      }
+    },
+    {
+      name: 'Jon Voight',
+      id: 'nm0000685',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BMTc2NTE3NDA3M15BMl5BanBnXkFtZTgwMDMyNTM1MjE@._V1_UY209_CR3,0,140,209_AL_.jpg',
+      characters: {
+        tt0311289: {
+          name: 'Mr. Sir',
+          link: '/title/tt0311289/characters/nm0000685?ref_=ttfc_fc_cl_t2'
+        },
+        tt0418279: {
+          name: 'Defense Secretary John Keller',
+          link: '/title/tt0418279/characters/nm0000685?ref_=ttfc_fc_cl_t7'
+        }
+      }
+    }
   ])
 
   return (
@@ -83,18 +79,18 @@ const ActorOverlap = () => {
               second={second}
             />
           ) : (
-              <Button
-                id="clear-button"
-                onClick={() => {
-                  setSearched(false)
-                  setData([])
-                  setFirst()
-                  setSecond()
-                }}
-              >
-                Start New Search
-          </Button>
-            )}
+            <Button
+              id="clear-button"
+              onClick={() => {
+                setSearched(false)
+                setData([])
+                setFirst()
+                setSecond()
+              }}
+            >
+              Start New Search
+            </Button>
+          )}
         </div>
         <div className="px-8" id="movie2">
           {!data.length && (
@@ -102,7 +98,7 @@ const ActorOverlap = () => {
           )}
         </div>
       </div>
-      <div class="poster-grid">
+      <div className="poster-grid">
         <div className="pt-8 pb-4" id="movie1poster">
           {first && (
             <>
@@ -113,7 +109,12 @@ const ActorOverlap = () => {
         </div>
         <div className="flex justify-center items-center" id="loader">
           {loading && <AniamtedReel />}
-          {!loading && searched && <div style={{ margin: '1rem 0'}}>Your search returned {data.length} {data.length === 1 ? 'result' : 'results'}</div>}
+          {!loading && searched && (
+            <div style={{ margin: '1rem 0' }}>
+              Your search returned {data.length}{' '}
+              {data.length === 1 ? 'result' : 'results'}
+            </div>
+          )}
         </div>
         <div className="pt-8 pb-4" id="movie2poster">
           {second && (
